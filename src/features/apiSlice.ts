51206c13db6a758d1ec5ee70e1248f7ee7157b94
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-03-12 21:13:47 -0400

    update auth response type

--- src/features/apiSlice.ts
+++ src/features/apiSlice.ts
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { LoginForm, UserResponse } from "../types";
+import { LoginForm, AuthResponse } from "../types";
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000" }),
     endpoints: (build) => ({
-        authenticateAccount: build.mutation<UserResponse, LoginForm>({
+        authenticateAccount: build.mutation<AuthResponse, LoginForm>({
             query: (credentials: LoginForm) => {
                 const formData = new URLSearchParams();
                 formData.append("username", credentials.username);
