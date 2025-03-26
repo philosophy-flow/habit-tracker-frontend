9007f2303713c8c9401a150ca291cf5433e751f9
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-03-26 11:51:23 -0500

    add variant style to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,14 @@
 type ButtonProps = {
     onClick?: VoidFunction;
     label: string;
+    variant?: string;
 };
 
-export default function Button({ onClick, label }: ButtonProps) {
+export default function Button({ onClick, label, variant = "" }: ButtonProps) {
     return (
         <button
             onClick={() => onClick?.()}
-            className="] mb-3 w-full rounded border border-[#009963] bg-transparent px-4 py-2 font-semibold hover:cursor-pointer hover:border-transparent hover:bg-[#009963] hover:text-white"
+            className={`mb-3 w-full rounded border ${variant === "secondary" ? "bg-transparent" : "bg-[#009963]"} border-[#009963] px-4 py-2 font-semibold hover:cursor-pointer hover:border-transparent hover:bg-[#009963] hover:text-white`}
         >
             {label}
         </button>
--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function BaseLayout() {
     return (
-        <main className="m-4 border border-red-600">
+        <main className="m-4">
             <Outlet />
         </main>
     );
