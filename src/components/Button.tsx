72508f9981e02edc8f05f845750b524b17c42b58
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-04-02 19:49:25 -0400

    add new button variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ onClick, label, variant = "" }: ButtonProps) {
     return (
         <button
             onClick={() => onClick?.()}
-            className={`mb-3 w-full rounded border ${variant === "secondary" ? "bg-transparent" : "bg-[#009963]"} border-[#009963] px-4 py-2 font-semibold hover:cursor-pointer hover:border-transparent hover:bg-[#009963] hover:text-white focus:border-[#FF4D8D] focus:ring-1 focus:ring-[#FF4D8D] focus:outline-none`}
+            className={`mb-3 w-full rounded border ${!variant && "bg-[#009963]"} ${variant === "secondary" && "bg-transparent"} ${variant === "dark" && "border-none bg-[#2E2E2E]"} border-[#009963] px-4 py-2 font-semibold hover:cursor-pointer hover:border-transparent hover:bg-[#009963] hover:text-white focus:border-[#FF4D8D] focus:ring-1 focus:ring-[#FF4D8D] focus:outline-none`}
         >
             {label}
         </button>
