08fffcf9543ecd220584fcbd347bd3570989ab71
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-04-12 21:12:19 -0400

    button component style refactor

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,10 +5,17 @@ type ButtonProps = {
 };
 
 export default function Button({ onClick, label, variant = "" }: ButtonProps) {
+    const variantSecondary = variant === "secondary" ? "bg-transparent " : "";
+    const variantDark = variant === "dark" ? "border-none bg-[#2E2E2E] " : "";
+
+    const additionalStyles = !variant
+        ? "bg-[#009963]"
+        : variantSecondary + variantDark;
+
     return (
         <button
             onClick={() => onClick?.()}
-            className={`mb-3 w-full rounded border ${!variant && "bg-[#009963]"} ${variant === "secondary" && "bg-transparent"} ${variant === "dark" && "border-none bg-[#2E2E2E]"} border-[#009963] px-4 py-2 font-semibold hover:cursor-pointer hover:border-transparent hover:bg-[#009963] hover:text-white focus-visible:border-[#FF4D8D] focus-visible:ring-1 focus-visible:ring-[#FF4D8D] focus-visible:outline-none`}
+            className={`mb-3 w-full rounded border border-[#009963] px-4 py-2 font-semibold hover:cursor-pointer hover:border-transparent hover:bg-[#009963] hover:text-white focus-visible:border-[#FF4D8D] focus-visible:ring-1 focus-visible:ring-[#FF4D8D] focus-visible:outline-none ${additionalStyles}`}
         >
             {label}
         </button>
