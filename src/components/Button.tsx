6735b1580240066dcd0c54a9f3ced51172ad03d9
Jacob Stewart <jacob.g.stewart@icloud.com>
2025-04-24 22:23:40 -0400

    add hover state for primary and secondary buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -17,7 +17,7 @@ export default function Button({
 }: ButtonProps) {
     const variantSecondary =
         variant === "secondary"
-            ? "bg-transparent border-2 border-[#009963] "
+            ? "bg-transparent border-2 border-[#009963] hover:shadow-[0_0_0_3px_rgba(28,177,122,0.4)] hover:bg-[rgba(28,177,122,0.05)] transition duration-150 ease-out"
             : "";
     const variantDark =
         variant === "dark" ? "bg-[#2E2E2E] border-2 border-transparent " : "";
@@ -27,10 +27,10 @@ export default function Button({
         variant === "tertiary" ? "border-2 border-[#63B3B2] " : "";
 
     const additionalStyles = !variant
-        ? "bg-[#009963] border-2 border-transparent "
+        ? "bg-[#009963] border-2 border-transparent hover:bg-[#1CB17A] hover:border-[#009963] transition duration-150 ease-out"
         : variantSecondary + variantDark + variantDanger + variantTertiary;
 
-    const buttonStyles = `mb-3 w-full rounded px-4 py-2 font-semibold hover:cursor-pointer focus-visible:border-2 focus-visible:border-[#FF4D8D] focus-visible:ring-1 focus-visible:ring-[#FF4D8D] focus-visible:outline-none ${additionalStyles} ${to ? "block text-center" : ""} ${className}`;
+    const buttonStyles = `mb-3 w-full rounded px-4 py-2 font-semibold cursor-pointer focus-visible:border-2 focus-visible:border-[#FF4D8D] focus-visible:ring-1 focus-visible:ring-[#FF4D8D] focus-visible:outline-none ${additionalStyles} ${to ? "block text-center" : ""} ${className}`;
 
     if (to) {
         return (
